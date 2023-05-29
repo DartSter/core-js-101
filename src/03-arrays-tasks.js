ed4bc52822e69fe418aed67cab43700a060a35fe
DartSter <kil-all@rambler.ru>
2023-05-29 16:03:30 +0300

    solved all tasks for arrays

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -552,8 +552,11 @@ function group(array, keySelector, valueSelector) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], (x) => x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.reduce((result, current) => {
+    const children = childrenSelector(current);
+    return result.concat(children);
+  }, []);
 }
 
 /**
@@ -568,8 +571,8 @@ function selectMany(/* arr, childrenSelector */) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  return indexes.reduce((acc, currentIndex) => acc[currentIndex], arr);
 }
 
 /**
@@ -590,8 +593,13 @@ function getElementByIndexes(/* arr, indexes */) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  if (arr.length < 2) return arr;
+  const midleIndex = Math.floor(arr.length / 2);
+  if (arr.length % 2 === 0) {
+    return [...arr.slice(-midleIndex), ...arr.slice(0, midleIndex)];
+  }
+  return [...arr.slice(-midleIndex), arr[midleIndex], ...arr.slice(0, midleIndex)];
 }
 
 module.exports = {
